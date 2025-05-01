2bd9fec6bf512ec04556e5a83ca63d70ec9634b3
jiminseong <iamjms4237@gachon.ac.kr>
2025-05-02 01:00:55 +0900

    fix:e -> 제거 및 에러 해결

--- src/modal/PhoneNumber.tsx
+++ src/modal/PhoneNumber.tsx
@@ -44,7 +44,7 @@ export default function PhoneNumberModal({ close }: PhoneNumberModalProps) {
       } else {
         alert("❌ 전화번호 등록에 실패했습니다. 다시 시도해주세요.");
       }
-    } catch (e) {
+    } catch {
       alert("❌ 예기치 못한 오류가 발생했습니다.");
     }
   };
